a246208e78c400e7aedee9ae95a373f1bb2d842e
Prateek Sharma <prateek.sharma10@ibm.com>
2025-02-19 08:34:59 +0530

    some changes

--- shopping/src/config/index.js
+++ shopping/src/config/index.js
@@ -1,4 +1,4 @@
-//const dotEnv = require("dotenv");
+const dotEnv = require("dotenv");
 
 // if (process.env.NODE_ENV !== "prod") {
 //   const configFile = `./.env.${process.env.NODE_ENV}`;
@@ -7,15 +7,13 @@
 //   dotEnv.config();
 // }
 
-// require('dotenv').config();
-// console.log("PORTttttttttt:", process.env.PORT);
-// console.log("DB_URLlllllll:", process.env.MONGODB_URI);
-// console.log("APP_SECRETtttttt:", process.env.APP_SECRET);
+require('dotenv').config();
+
 module.exports = {
-  PORT: "8003",
-  DB_URL: "mongodb+srv://1022prateeksharma:!1pRateek@shoppingcluster223.gd5ef.mongodb.net/collection?retryWrites=true&w=majority&appName=ShoppingCluster223",
-  APP_SECRET: "elkmf",
-  MSG_QUEUE_URL: 'amqp://myuser:mypassword@rabbitmq:5672',
+  PORT: process.env.PORT,
+  DB_URL: process.env.MONGODB_URI,
+  APP_SECRET: process.env.APP_SECRET,
+  MSG_QUEUE_URL: process.env.RABBITMQ_URL,
   EXCHANGE_NAME: 'ONLINE_SHOPPING',
   SHOPPING_BINDING_KEY: 'SHOPPING_SERVICE',
   CUSTOMER_BINDING_KEY: 'CUSTOMER_SERVICE',
